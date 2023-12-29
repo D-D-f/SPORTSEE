9f891bb361de4948700cd8d99a47f6f638426bdc
D-D-f <defreitas-david@outlook.fr>
2023-12-29 15:17:06 +0100

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
-import Header from "./Component/Header/Header";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 const App = () => {
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:3000/user/12/performance",
+    fetcher
+  );
+  if (error) throw new Error("Unable to recover data");
+  if (isLoading) return "Loading";
+
+  console.log(data);
+
   return (
     <>
-      <Header />
+      <main></main>
     </>
   );
 };
