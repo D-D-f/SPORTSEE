09c82dbbbc1985bb79329b5060a6115fec47b321
D-D-f <defreitas-david@outlook.fr>
2024-01-13 22:20:09 +0100

    add component

--- src/Component/IconActivty/IconActivityScore.tsx
+++ src/Component/IconActivty/IconActivityScore.tsx
@@ -1,5 +1,14 @@
+import IconScore from "../IconScore/IconScore.tsx";
+
 const IconActivityScore = () => {
 
+
+
+    return (
+        <div>
+            <IconScore />
+        </div>
+    )
 }
 
 export default IconActivityScore;
