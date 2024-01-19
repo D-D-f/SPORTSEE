c8ae4ccb04b8ea8c9e05ae98db392de628c45902
D-D-f <defreitas-david@outlook.fr>
2024-01-19 17:14:53 +0100

    update typescript

--- src/Component/Chart/Chart.tsx
+++ src/Component/Chart/Chart.tsx
@@ -13,18 +13,17 @@ const Chart = () => {
                 data={data.data?.sessions}
                 barSize={20}
             >
-                <Legend verticalAlign='top' align='right' iconType='circle' wrapperStyle={{marginTop: '-33px'}}/>
-                <CartesianGrid strokeDasharray="1" vertical={false}/>
-                <XAxis dataKey="day" scale="point" padding={{left: 10, right: 10}}/>
-                <XAxis dataKey='day' tickLine={false} axisLine={false}/>
-                <XAxis dataKey='calories' type='number' tickLine={false} axisLine={false}/>
-                <YAxis dataKey='kilogram' type='number' tickLine={false} orientation='right'
-                       domain={['dataMin - 1', 'dataMax + 1']}/>
-                <YAxis dataKey='calories' type='number' yAxisId='calorie' hide/>
-                <Tooltip/>
-                <Legend/>
-                <Bar barSize={10} name="Poids (kg)" dataKey="kilogram" fill="#E60000" radius={[10, 10, 0, 0]}/>
-                <Bar barSize={10} name="Calories brûlées (kCal)" dataKey="calories" radius={[10, 10, 0, 0]}/>
+                <Legend verticalAlign='top' align='right' iconType='circle' wrapperStyle={{marginTop: '-33px'}} />
+                <CartesianGrid strokeDasharray='3 3' vertical={false} />
+                <XAxis dataKey="day" tickLine={false} scale="point" padding={{left: 50, right: 50}} />
+                <XAxis dataKey='day' tickLine={false} axisLine={false} />
+                <XAxis dataKey='calories' type='number' tickLine={false} axisLine={false} />
+                <YAxis dataKey='kilogram' type='number' tickLine={false} axisLine={false}  orientation='right'
+                       domain={['dataMin - 1', 'dataMax + 1']} />
+                <YAxis dataKey='calories' type='number' yAxisId='calorie'  />
+                <Tooltip />
+                <Bar barSize={10} name="Poids (kg)" dataKey="kilogram" fill="#E60000" radius={[10, 10, 0, 0]} />
+                <Bar barSize={10} name="Calories brûlées (kCal)" dataKey="calories" radius={[10, 10, 0, 0]} />
             </BarChart>
         </ResponsiveContainer>
     )
