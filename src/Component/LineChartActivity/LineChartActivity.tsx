2b7c0510448f6b009c92a79a7ead5ddf6ba09961
D-D-f <defreitas-david@outlook.fr>
2024-01-23 11:30:17 +0100

    update legend linechart

--- src/Component/LineChartActivity/LineChartActivity.tsx
+++ src/Component/LineChartActivity/LineChartActivity.tsx
@@ -4,14 +4,24 @@ import CustomizedTooltipSession from "../../CustomChart/CustomizedTooltipSession
 
 const LineChartActivity = () => {
     const [data] = UseCallApiSession();
-    console.log(data)
+    const numberDay = data?.data?.sessions;
+    const days: string[] = ["L", "M", "M", "J", "V", "S", "D"];
+
+    if(numberDay) {
+        days.forEach((day: string, index: number) => {
+            Object.defineProperty(numberDay[index], 'letterDay', {
+                value: day,
+                writable: false
+            })
+        })
+    }
 
     return (
-        <ResponsiveContainer width="100%" height="100%" style={{backgroundColor:"var(--red)"}}>
+        <ResponsiveContainer width="100%" height="100%">
             <LineChart width={258} height={263} data={data?.data?.sessions}
                        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                 <CartesianGrid strokeDasharray="0 0" />
-                <XAxis dataKey="day" />
+                <XAxis dataKey="letterDay" />
                 <Tooltip content={<CustomizedTooltipSession />}  />
                 <Line type="monotone" dataKey="sessionLength" stroke="white" />
             </LineChart>
