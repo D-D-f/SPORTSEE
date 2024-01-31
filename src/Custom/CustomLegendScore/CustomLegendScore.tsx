913a55f2ddbeb0ad23081af4192ac9fe0b5ec989
D-D-f <defreitas-david@outlook.fr>
2024-01-31 09:58:30 +0100

    update legendscore

--- src/Custom/CustomLegendScore/CustomLegendScore.tsx
+++ src/Custom/CustomLegendScore/CustomLegendScore.tsx
@@ -2,10 +2,10 @@ const CustomLegendScore = ({payload}) => {
     return (
         <div style={{height: "300px"}}>
             <p style={{position: "absolute", top: "20px", left: "20px", fontWeight: "bold", fontSize: "20px"}}>Score</p>
-            <p style={{position: "absolute", top: "130px", width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+            <p style={{fontWeight: "bold", fontSize: "25px", position: "absolute", top: "130px", width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
                 {payload[1]?.payload.todayScore}%
-                <span style={{display: "block"}}>de votre
-                <span style={{display: "block"}}>
+                <span style={{fontWeight: "normal", fontSize: "15px", display: "block", marginTop: "5px", color: "var(--fontColorChart"}}>de votre
+                <span style={{fontWeight: "normal", fontSize: "15px", display: "block", marginTop: "5px", color: "var(--fontColorChart"}}>
                     objectif
                 </span></span>
             </p>
