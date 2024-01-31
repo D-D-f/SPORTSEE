3328493f180351256c2e892b48abc94fee4c6fbc
D-D-f <defreitas-david@outlook.fr>
2024-01-31 09:51:51 +0100

    update radialbartchart

--- src/Component/RadialBarChartObjective/RadialBarChartObjective.tsx
+++ src/Component/RadialBarChartObjective/RadialBarChartObjective.tsx
@@ -15,19 +15,20 @@ const RadialBarChartObjective = ({dataOriginal}) => {
 
         newData.push({
             userId: data.id,
-            todayScore: score * 100
-        })
-        newData.push({
-            userId: data.id,
             todayScore: 100,
             fill: '#ffffff00'
         })
+        newData.push({
+            userId: data.id,
+            todayScore: score * 100,
+        })
+
     }
 
     return (
         <ResponsiveContainer width={"30%"} height={300} style={{backgroundColor: "var(--backgroundGray)", borderRadius: "5px"}}>
-            <RadialBarChart startAngle={140} endAngle={500} cx='50%' cy='50%' innerRadius={70} barSize={10} outerRadius={120} data={newData} >
-                <RadialBar cornerRadius='50%' dataKey='todayScore' fill='#E60000' style={{border: "1Px solid blue"}} />
+            <RadialBarChart startAngle={140} endAngle={500} barSize={10} outerRadius={180} data={newData} >
+                <RadialBar dataKey='todayScore' fill="#E60000" background />
                 <Legend content={<CustomLegendScore />} />
             </RadialBarChart>
         </ResponsiveContainer>
--- src/Custom/CustomLegendScore/CustomLegendScore.tsx
+++ src/Custom/CustomLegendScore/CustomLegendScore.tsx
@@ -3,8 +3,8 @@ const CustomLegendScore = ({payload}) => {
     return (
         <div style={{height: "300px"}}>
             <p style={{position: "absolute", top: "20px", left: "20px", fontWeight: "bold", fontSize: "20px"}}>Score</p>
-            <p style={{position: "absolute", top: "150px", width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
-                {payload[0]?.payload.todayScore}%
+            <p style={{position: "absolute", top: "130px", width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+                {payload[1]?.payload.todayScore}%
                 <span style={{display: "block"}}>de votre
                 <span style={{display: "block"}}>
                     objectif
