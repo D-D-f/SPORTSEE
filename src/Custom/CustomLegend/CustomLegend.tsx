1b14605c6e597486bf96d1d1c9d6225d41aa0204
D-D-f <defreitas-david@outlook.fr>
2024-02-06 08:48:09 +0100

    add bold style

--- src/Custom/CustomLegend/CustomLegend.tsx
+++ src/Custom/CustomLegend/CustomLegend.tsx
@@ -1,17 +1,25 @@
 import "./CustomLegend.css";
-const CustomLegend = (props: {payload: [] }) => {
-    const {payload} = props;
+const CustomLegend = (props: { payload: [] }) => {
+  const { payload } = props;
 
-    return (
-        <div className="customLegend">
-            <p className="custom">Activité quotidienne</p>
-            <ul className="customLegendUl">
-                {payload.map((value: {payload: {name: string}}, index: number) => {
-                    return <li className={`legendStyle-${index}`} key={index}><span style={{color: "var(--fontColorChart"}}>{value.payload.name}</span></li>
-                })}
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className="customLegend">
+      <p style={{ fontWeight: "bold" }} className="custom">
+        Activité quotidienne
+      </p>
+      <ul className="customLegendUl">
+        {payload.map((value: { payload: { name: string } }, index: number) => {
+          return (
+            <li className={`legendStyle-${index}`} key={index}>
+              <span style={{ color: "var(--fontColorChart" }}>
+                {value.payload.name}
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default CustomLegend;
